435f5f9524958bf994470caf248da6c75152ae35
Loki M <lipho@plus20charisma.com>
2015-05-05 09:48:52 -0700

    updated to python 32 & fixed spelling error

--- reposter.py
+++ reposter.py
@@ -32,47 +32,47 @@ r.login(USERNAME, PASSWORD)
 def rep_bot_coms():
 	user = r.get_redditor(USERSCRAPE)
 	comments = user.get_comments(limit=MAXPOSTS)
-	print "Checking comments..."
+	print ("Checking comments...")
 	for comment in comments:
 		#checks if comment already exists in db
 		cur.execute('SELECT * FROM oldcomments WHERE ID=?', [comment.id])
 		if not cur.fetchone():
 			try:
 				if comment.score > KARMATHRESHOLD:
-					print "Reposting comment above Karma Threshold..."
+					print ("Reposting comment above Karma Threshold...")
 					repost_comment_body = comment.body
 					#since comments don't have titles, the first 10 words become post title
 					repost_title_list = repost_comment_body.split()
 					repost_title = ' '.join(repost_title_list[:10]) + "..."
 					r.submit(SUBREDDIT, repost_title, repost_comment_body+"\n > I'm a bot that is reposting from /u/%s. If there's any trouble, please message /u/plus20charisma!" % USERSCRAPE)
-					print "Comment posted to /r/"+SUBREDDIT+"!"
+					print ("Comment posted to /r/"+SUBREDDIT+"!")
 			except AttributeError:
 				pass
 			#writes comment to db by id
 			cur.execute('INSERT INTO oldcomments VALUES(?)', [comment.id])
 			sql.commit()
-	print "No more comments to submit!"
+	print ("No more comments to submit!")
 
 #same as rep_bot_coms() but for submissions
 def rep_bot_submitted():
 	user = r.get_redditor(USERSCRAPE)	
 	posts = user.get_submitted(limit=MAXPOSTS)
-	print "Checking submissions..."
+	print ("Checking submissions...")
 	for post in posts:
 		cur.execute('SELECT * FROM oldsubmissions WHERE ID=?', [post.id])
 		if not cur.fetchone():
 			try:
 				if post.score > KARMATHRESHOLD:
-					print "Reposting submission above Karma Threshold..."
+					print ("Reposting submission above Karma Threshold...")
 					repost_title = post.title
 					repost_url = post.url
 					r.submit(SUBREDDIT, repost_title, repost_url+"\n > I'm a bot that is reposting from /u/%s. If there's any trouble, please message /u/plus20charisma!" % USERSCRAPE)
-					print "Sumission posted to /r/"+SUBREDDIT+"!"
+					print ("Submission posted to /r/"+SUBREDDIT+"!")
 			except AttributeError:
 				pass
 			cur.execute('INSERT INTO oldsubmissions VALUES(?)', [post.id])
 			sql.commit()
-	print "No more submissions to submit!"
+	print ("No more submissions to submit!")
 
 while True:
 	rep_bot_submitted()
